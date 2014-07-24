db30462610f5af99af075d02a21db54fd85dcd4c
Fredrik Liden <admin@lidenlab.com>
2014-07-24 12:00:36 -0700

    Add example for multipart upload

--- client/src/test/java/net/sf/okapi/acorn/client/HelloWorldServiceTest.java
+++ client/src/test/java/net/sf/okapi/acorn/client/HelloWorldServiceTest.java
@@ -1,11 +1,18 @@
 package net.sf.okapi.acorn.client;
 
+import java.io.IOException;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.glassfish.jersey.media.multipart.BodyPart;
+import org.glassfish.jersey.media.multipart.MultiPart;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.junit.Test;
 
 public class HelloWorldServiceTest {
@@ -32,6 +39,34 @@ public class HelloWorldServiceTest {
 		System.out.println("Output from Server .... \n");
 		System.out.println(output);
 		
+		response.close();
+	}
+	
+	@Test
+	public void testMultiPart() throws IOException{
+		
+	    MultiPart multiPart = new MultiPart().
+	  	      bodyPart(new BodyPart("Body Part 1", MediaType.TEXT_PLAIN_TYPE)).
+	  	      bodyPart(new BodyPart("Body Part 2", MediaType.TEXT_PLAIN_TYPE));
+		
+		Client client = ClientBuilder.newClient().register(MultiPartFeature.class);
+		WebTarget target = client.target(restURL).path("hello");
+		
+		System.out.println(multiPart.getMediaType());
+		
+		Response response = target.request().post(Entity.entity(multiPart, multiPart.getMediaType()));
+		
+		if (response.getStatus() != 200) {
+			   throw new RuntimeException("Failed : HTTP error code : "
+				+ response.getStatus());
+		}
+		
+		String output = response.readEntity(String.class);
+		 
+		System.out.println("Output from Server .... \n");
+		System.out.println(output);
+		
+		multiPart.close();
 		response.close();
 	}
 }
--- server/src/main/java/net/sf/okapi/acorn/rest/HelloWorldService.java
+++ server/src/main/java/net/sf/okapi/acorn/rest/HelloWorldService.java
@@ -1,10 +1,16 @@
 package net.sf.okapi.acorn.rest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.media.multipart.BodyPart;
+import org.glassfish.jersey.media.multipart.MultiPart;
 
 @Path("/hello")
 public class HelloWorldService {
@@ -19,5 +25,17 @@ public class HelloWorldService {
 		
 		return Response.status(200).entity(output).build();
  
+	}
+	
+	@POST
+	@Consumes("multipart/mixed")
+	public Response postForm(MultiPart multiPart) {
+		
+		for (BodyPart bp : multiPart.getBodyParts()) {
+			String bpStr = bp.getEntityAs(String.class);
+			System.out.println(bpStr);			
+		}
+		
+		return Response.status(200).entity("Processed multipart").build();
 	}
 }
