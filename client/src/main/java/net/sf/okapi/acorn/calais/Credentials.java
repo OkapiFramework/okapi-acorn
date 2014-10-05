47caca6ed250eceabb0d443076956fba2f74f990
Yves <yves.savourel@gmail.com>
2014-10-05 07:16:53 -0600

    Added missing file.

---
+++ client/src/main/java/net/sf/okapi/acorn/calais/Credentials.java
@@ -0,0 +1,15 @@
+package net.sf.okapi.acorn.calais;
+
+public class Credentials {
+
+	private String licenseId;
+	
+	public Credentials () {
+		licenseId = "qp4vk8cbb4xjmjde6hzpuxdq";
+	}
+	
+	public String getLicenseId () {
+		return licenseId;
+	}
+
+}
